b745f2e6b387c0cf6c1018883030c4914be8b386
Ismail CHOUHAD <ismailchou8@gmail.com>
2023-03-28 15:05:18 +0000

    responsive design

--- src/Pages/Stock.js
+++ src/Pages/Stock.js
@@ -1,62 +1,99 @@
-import { Alert, Button, Grid, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField } from "@mui/material";
+import {
+  Alert,
+  Button,
+  Grid,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TextField,
+} from "@mui/material";
 import axios from "axios";
 import { Fragment, useState } from "react";
+import { PropagateLoader } from "react-spinners";
 import SideBar from "../Components/SideBar/SideBar";
 
 const Stock = () => {
-  const buttonStyle = {  marginTop: "10px" }
-  const [token,setToken] = useState('');
-  const [sku,setSKU] = useState('');
-  const [stock,setStock] = useState([]);
+  const buttonStyle = { marginTop: "10px" };
+  const [token, setToken] = useState("");
+  const [sku, setSKU] = useState("");
+  const [stock, setStock] = useState([]);
   const [showTable, setShowTable] = useState(false);
   const [errorAlert, setErrorAlert] = useState(false);
-  const [errorMessage, setErrorMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
   const data = {
-    token : `${token}`,
-    sku : `${sku}`
+    token: `${token}`,
+    sku: `${sku}`,
   };
 
   const handleClick = () => {
-    setIsLoading(true)
+    setIsLoading(true);
     // console.log( `${token} and ${sku}`);
-    const config ={
-      method : 'post',
-      baseURL : 'http://localhost/my-website/wp-json/choc-api/v1',
-      data : data
-    }
-    axios.request('/get-stock',config)
-    .then(res => {
-      setShowTable(true);
-      setErrorAlert(false);
-      // console.log(res.data);
-      setStock(res.data);
-    })
-    .catch(err => {
-      setShowTable(false);
-      setErrorAlert(true);
-      // console.log(err.response.data.message);
-      setErrorMessage(err.response.data.message)
-      setStock([]);
-    })
-    .finally(() => {
-      setIsLoading(false);
-    })
+    const config = {
+      method: "post",
+      baseURL: "http://localhost/my-website/wp-json/choc-api/v1",
+      data: data,
+    };
+    axios
+      .request("/get-stock", config)
+      .then((res) => {
+        setShowTable(true);
+        setErrorAlert(false);
+        // console.log(res.data);
+        setStock(res.data);
+      })
+      .catch((err) => {
+        setShowTable(false);
+        setErrorAlert(true);
+        if (err.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          setErrorMessage(err.response.data.message);
+          // console.log(err.response.data.message);
+        } else if (err.request) {
+          // The request was made but no response was received
+          setErrorMessage(
+            "The server is currently unavailable. Please try again later."
+          );
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          setErrorMessage(
+            "An error occurred while processing your request. Please try again later."
+          );
+        }
+        setStock([]);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
   };
 
   return (
     <div className="parent">
       <div className="child-1">
-        <SideBar/>
+        <SideBar />
       </div>
+      {isLoading && (
+        <div className="isLoading">
+          <PropagateLoader
+            color={"#123abc"}
+            loading={true}
+            css={{ backgroundColor: "yellow", width: "30px", height: "30px" }}
+          />
+        </div>
+      )}
       <div className="child-2">
-        {errorAlert ? <Alert severity="error">{errorMessage}</Alert> : <></> }
+        {errorAlert ? <Alert severity="error">{errorMessage}</Alert> : <></>}
         <div className="child-2-1">
           <Fragment>
-            <Grid container className='content-zone'>
+            <Grid container className="content-zone">
               <Grid item xs={9} sm={9}>
-                  <TextField
+                <TextField
                   required
                   id="token"
                   name="token"
@@ -65,9 +102,10 @@ const Stock = () => {
                   autoComplete="given-name"
                   variant="standard"
                   value={token}
-                  onChange={(e)=>setToken(e.target.value)}/>
+                  onChange={(e) => setToken(e.target.value)}
+                />
 
-                  <TextField
+                <TextField
                   required
                   id="sku"
                   name="sku"
@@ -76,51 +114,49 @@ const Stock = () => {
                   autoComplete="given-name"
                   variant="standard"
                   value={sku}
-                  onChange={(e)=>setSKU(e.target.value)}/>
-                  
+                  onChange={(e) => setSKU(e.target.value)}
+                />
               </Grid>
-              <Grid item xs={12} style={buttonStyle}>
-                  <Button variant="contained" onClick={handleClick}>
-                    {isLoading ? <h5>Loading ...</h5> : <h5>Get Stock</h5>}
-                  </Button>
+              <Grid className="content-zone" item xs={12} style={buttonStyle}>
+                <Button variant="contained" onClick={handleClick}>
+                  {isLoading ? <h5>Loading ...</h5> : <h5>Get Stock</h5>}
+                </Button>
               </Grid>
             </Grid>
           </Fragment>
         </div>
         <div className="child-2-2">
           <Fragment>
-            <Grid container className='content-zone'>
+            <Grid container className="content-zone">
               {showTable && (
-              <TableContainer component={Paper} className='table'>
-                <Table sx={{ minWidth: 90 }} aria-label="simple table">
-                  <TableHead>
+                <TableContainer component={Paper} className="table">
+                  <Table sx={{ minWidth: 90 }} aria-label="simple table">
+                    <TableHead>
                       <TableRow>
-                        <TableCell ><b>Stock status</b></TableCell>
-                        <TableCell ><b>Stock quantity</b></TableCell>
+                        <TableCell>
+                          <b>Stock status</b>
+                        </TableCell>
+                        <TableCell>
+                          <b>Stock quantity</b>
+                        </TableCell>
                       </TableRow>
-                  </TableHead>
-                  <TableBody>
-                    {
-                    stock.map(item => (
-                    <TableRow key={item}>
-                      <TableCell>
-                          {item.stock_status}
-                      </TableCell>
-                      <TableCell>
-                          {item.stock_quantity}
-                      </TableCell>
-                    </TableRow>
-                    ))
-                    }
-                  </TableBody>
-                </Table>
-              </TableContainer>
+                    </TableHead>
+                    <TableBody>
+                      {stock.map((item) => (
+                        <TableRow key={item}>
+                          <TableCell>{item.stock_status}</TableCell>
+                          <TableCell>{item.stock_quantity}</TableCell>
+                        </TableRow>
+                      ))}
+                    </TableBody>
+                  </Table>
+                </TableContainer>
               )}
             </Grid>
           </Fragment>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 export default Stock;
--- src/Pages/UpdateStock.js
+++ src/Pages/UpdateStock.js
@@ -1,67 +1,104 @@
-import { Alert, Button, Grid, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField } from "@mui/material";
+import {
+  Alert,
+  Button,
+  Grid,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TextField,
+} from "@mui/material";
 import axios from "axios";
 import { Fragment, useState } from "react";
+import { PropagateLoader } from "react-spinners";
 import SideBar from "../Components/SideBar/SideBar";
 
 const UpdateStock = () => {
-  const buttonStyle = {  marginTop: "10px" }
-  const [token,setToken] = useState('');
-  const [sku,setSKU] = useState('');
-  const [action,setAction] = useState('');
-  const [valeur,setValeur] = useState(null)
-  const [updateStock,setUpdateStock] = useState([]);
+  const buttonStyle = { marginTop: "10px" };
+  const [token, setToken] = useState("");
+  const [sku, setSKU] = useState("");
+  const [action, setAction] = useState("");
+  const [valeur, setValeur] = useState(null);
+  const [updateStock, setUpdateStock] = useState([]);
   const [showTable, setShowTable] = useState(false);
   const [errorAlert, setErrorAlert] = useState(false);
-  const [errorMessage, setErrorMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState("");
 
   const [isLoading, setIsLoading] = useState(false);
 
   const data = {
-    token : `${token}`,
-    sku : `${sku}`,
-    action : `${action}`,
-    valeur : parseInt(valeur)
+    token: `${token}`,
+    sku: `${sku}`,
+    action: `${action}`,
+    valeur: parseInt(valeur),
   };
 
   const handleClick = () => {
     setIsLoading(true);
     // console.log( `${token} and ${sku}`);
-    const config ={
-      method : 'post',
-      baseURL : 'http://localhost/my-website/wp-json/choc-api/v1',
-      data : data
-    }
-    axios.request('/update-stock',config)
-    .then(res => {
-      setShowTable(true);
-      setErrorAlert(false);
-      // console.log(res.data);
-      setUpdateStock(res.data);
-    })
-    .catch(err => {
-      setShowTable(false)
-      setErrorAlert(true)
-      // console.log(err.response.data.message);
-      setErrorMessage(err.response.data.message)
-      setUpdateStock([]);
-    })
-    .finally(() => {
-      setIsLoading(false);
-    })
+    const config = {
+      method: "post",
+      baseURL: "http://localhost/my-website/wp-json/choc-api/v1",
+      data: data,
+    };
+    axios
+      .request("/update-stock", config)
+      .then((res) => {
+        setShowTable(true);
+        setErrorAlert(false);
+        // console.log(res.data);
+        setUpdateStock(res.data);
+      })
+      .catch((err) => {
+        setShowTable(false);
+        setErrorAlert(true);
+        if (err.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          setErrorMessage(err.response.data.message);
+          // console.log(err.response.data.message);
+        } else if (err.request) {
+          // The request was made but no response was received
+          setErrorMessage(
+            "The server is currently unavailable. Please try again later."
+          );
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          setErrorMessage(
+            "An error occurred while processing your request. Please try again later."
+          );
+        }
+        setUpdateStock([]);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
   };
 
   return (
     <div className="parent">
       <div className="child-1">
-        <SideBar/>
+        <SideBar />
       </div>
+      {isLoading && (
+        <div className="isLoading">
+          <PropagateLoader
+            color={"#123abc"}
+            loading={true}
+            css={{ backgroundColor: "yellow", width: "30px", height: "30px" }}
+          />
+        </div>
+      )}
       <div className="child-2">
-        {errorAlert ? <Alert severity="error">{errorMessage}</Alert> : <></> }
+        {errorAlert ? <Alert severity="error">{errorMessage}</Alert> : <></>}
         <div className="child-2-1">
           <Fragment>
-            <Grid container className='content-zone'>
+            <Grid container className="content-zone">
               <Grid item xs={9} sm={9}>
-                  <TextField
+                <TextField
                   required
                   id="token"
                   name="token"
@@ -70,9 +107,10 @@ const UpdateStock = () => {
                   autoComplete="given-name"
                   variant="standard"
                   value={token}
-                  onChange={(e)=>setToken(e.target.value)}/>
+                  onChange={(e) => setToken(e.target.value)}
+                />
 
-                  <TextField
+                <TextField
                   required
                   id="sku"
                   name="sku"
@@ -81,9 +119,10 @@ const UpdateStock = () => {
                   autoComplete="given-name"
                   variant="standard"
                   value={sku}
-                  onChange={(e)=>setSKU(e.target.value)}/>
-                  
-                  <TextField
+                  onChange={(e) => setSKU(e.target.value)}
+                />
+
+                <TextField
                   required
                   id="action"
                   name="action"
@@ -92,9 +131,10 @@ const UpdateStock = () => {
                   autoComplete="given-name"
                   variant="standard"
                   value={action}
-                  onChange={(e)=>setAction(e.target.value)}/>
+                  onChange={(e) => setAction(e.target.value)}
+                />
 
-                  <TextField
+                <TextField
                   required
                   id="value"
                   name="value"
@@ -103,51 +143,49 @@ const UpdateStock = () => {
                   autoComplete="given-name"
                   variant="standard"
                   value={valeur}
-                  onChange={(e)=>setValeur(e.target.value)}/>
-
+                  onChange={(e) => setValeur(e.target.value)}
+                />
               </Grid>
-              <Grid item xs={12} style={buttonStyle}>
-                  <Button variant="contained" onClick={handleClick}>
-                    {isLoading ? <h5>Loading ...</h5> : <h5>Update Stock</h5>}
-                  </Button>
+              <Grid className="content-zone" item xs={12} style={buttonStyle}>
+                <Button variant="contained" onClick={handleClick}>
+                  {isLoading ? <h5>Loading ...</h5> : <h5>Update Stock</h5>}
+                </Button>
               </Grid>
             </Grid>
           </Fragment>
         </div>
         <div className="child-2-2">
           <Fragment>
-            <Grid container className='content-zone'>
+            <Grid container className="content-zone">
               {showTable && (
-              <TableContainer component={Paper} className='table'>
-                <Table sx={{ minWidth: 90 }} aria-label="simple table">
-                  <TableHead>
+                <TableContainer component={Paper} className="table">
+                  <Table sx={{ minWidth: 90 }} aria-label="simple table">
+                    <TableHead>
                       <TableRow>
-                        <TableCell ><b>Stock status</b></TableCell>
-                        <TableCell ><b>Stock quantity</b></TableCell>
+                        <TableCell>
+                          <b>Stock status</b>
+                        </TableCell>
+                        <TableCell>
+                          <b>Stock quantity</b>
+                        </TableCell>
                       </TableRow>
-                  </TableHead>
-                  <TableBody>
-                    {
-                    updateStock.map(item => (
-                    <TableRow key={item}>
-                      <TableCell>
-                          {item.stock_status}
-                      </TableCell>
-                      <TableCell>
-                          {item.stock_quantity}
-                      </TableCell>
-                    </TableRow>
-                    ))
-                    }
-                  </TableBody>
-                </Table>
-              </TableContainer>
+                    </TableHead>
+                    <TableBody>
+                      {updateStock.map((item) => (
+                        <TableRow key={item}>
+                          <TableCell>{item.stock_status}</TableCell>
+                          <TableCell>{item.stock_quantity}</TableCell>
+                        </TableRow>
+                      ))}
+                    </TableBody>
+                  </Table>
+                </TableContainer>
               )}
             </Grid>
           </Fragment>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 export default UpdateStock;
